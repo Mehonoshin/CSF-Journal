f7b80a7f4d1f73e9eb6a33efa4605b4757a58489
Stanislav Mekhonoshin <mehonoshin@sc.vsu.ru>
2011-02-12 17:18:28 +0300

    added ExtJS tree displaying groups and users in groups

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,35 @@
 class MainController < ApplicationController
   def index
   end
+  
+  def tree
+  end
+  
+  def gettree
+    groups = Group.find(params['node']).groups
+    json = '['
+    i = 0
+    
+    if groups.empty?
+      users = Group.find(params['node']).users
+      users.each do |g|
+        if i != 0
+          json << ','
+        end
+        i += 1
+        json << '{"text":"' + g.username + '","id":"' + g.id.to_s + '", "leaf" : "true"}'
+      end      
+    else
+      groups.each do |g|
+        if i != 0
+          json << ','
+        end
+        i += 1
+        json << '{"text":"' + g.description + '","id":"' + g.id.to_s + '"}'
+      end
+    end
+    
+    json << ']'
+    render :text => json
+  end
 end
