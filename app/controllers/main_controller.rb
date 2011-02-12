f509d14cee6ae9a70bac99f20664bb852746fd37
Stanislav Mekhonoshin <mehonoshin@sc.vsu.ru>
2011-02-12 19:41:32 +0300

    A bit modified JS Tree. Ability to move user between groups

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,16 +10,7 @@ class MainController < ApplicationController
     json = '['
     i = 0
     
-    if groups.empty?
-      users = Group.find(params['node']).users
-      users.each do |g|
-        if i != 0
-          json << ','
-        end
-        i += 1
-        json << '{"text":"' + g.username + '","id":"' + g.id.to_s + '", "leaf" : "true"}'
-      end      
-    else
+    if not groups.empty?
       groups.each do |g|
         if i != 0
           json << ','
@@ -28,8 +19,33 @@ class MainController < ApplicationController
         json << '{"text":"' + g.description + '","id":"' + g.id.to_s + '"}'
       end
     end
+    users = Group.find(params['node']).users
+    users.each do |g|
+      if i != 0
+        json << ','
+      end
+      i += 1
+      json << '{"text":"' + g.username + '","id":"' + g.id.to_s + '", "leaf" : "true"}'
+    end      
     
     json << ']'
     render :text => json
   end
+  
+  
+  def move
+    if params['leaf'] != ''
+      u = User.find(params['nodeid'])
+      u.groups.delete(Group.find(params['oldparentid']))
+      u.groups << Group.find(params['newparentid'])
+      u.save
+    else
+      g = Group.find(params['nodeid'])
+      if (g.group_id.to_s.eql?(params['oldparentid']))
+        g.group_id = params['newparentid']
+        g.save
+      end
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Csf::Application.routes.draw do
   root :to => "main#index"
   match 'tree' => "main#tree"
   match 'gettree' => "main#gettree"
+  match 'move' => "main#move"  
 end
