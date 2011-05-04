a79da34cddcd8fd3d697af1870d8725b20cce03a
Stanislav Mekhonoshin <mehonoshin@sc.vsu.ru>
2011-05-04 13:27:56 +0400

    Control commit :)

--- public/javascripts/use.js
+++ public/javascripts/use.js
@@ -1,6 +1,31 @@
+var prevVal = null;
+
 $(document).ready(function() {
 	$('#add-mark-btn').click(function() {
-		$("#marks-table tr:first").append("<th>X</th>");
-		$('#marks-table tr:not(:first)').append('<td>Row</td>')
+		var today = new Date();
+     	date = today.getDate() + '.'+ (today.getMonth() + 1) + '.' + today.getFullYear();     
+		$("#marks-table tr:first").append('<th>' + date + '</th>');
+		$('#marks-table tr:not(:first)').append('<td class="marks-table-cell">asd</td>')
+	});
+	
+	// $('.marks-table-cell').dblclick(function() {
+	// 	alert("dbl!");
+	// });
+
+	$('.marks-table-cell').click(function() {
+		if (!$(this).hasClass('blocked')) {
+			prevVal = $(this).html();
+			$(this).html('');
+			$(this).addClass('blocked');
+			$(this).append('<input class="mark-edit-field" type="text" size="5" value="' + prevVal + '">');			
+		}
+	});
+	
+	$('.marks-table-cell').mouseleave(function() {
+		if ($(this).hasClass('blocked')) {
+			val = $(this).children('input').attr('value');
+			$(this).html(val);
+			$(this).removeClass('blocked');
+		}
 	});
 });
